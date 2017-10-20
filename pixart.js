4341b5fd503b40718f4361703c1db2e540192a8b
Lyle Innes <lyleinnes@Lyles-MacBook-Pro.local>
2017-10-20 15:44:23 +1100

    time up! got to commit 7

--- pixart.js
+++ pixart.js
@@ -1,8 +1,10 @@
 $colorField = $('#color-field');
 $setColor = $('#set-color');
+
+$movieField = $('#movie-field');
+$setMovie = $('#set-movie');
+
 $brush = $('.brush');
-
-
 
 $setColor.on('click', function() {
   event.preventDefault();
@@ -15,8 +17,20 @@ for (i = 0; i < 1000; i++) {
 
 var body = document.querySelector('body');
 
-body.addEventListener('click', function(event) {
+body.addEventListener('mouseover', function(event) {
     if (event.target.className === 'square') {
       event.target.style.background = $brush.css('background')
     }
+});
+
+$setMovie.on('click', function() {
+  event.preventDefault();
+  $.ajax({
+      url: 'http://www.omdbapi.com/?apikey=2f6435d9&',
+      method: 'post',
+      data: { apikey: '2f6435d9', t: $movieField}
+    }).done(function(response) {
+      var $likesCount = $(event.target).closest('.ui-card').find('.likes-count');
+      $likesCount.text(response.likes_count)
+    })
 });
