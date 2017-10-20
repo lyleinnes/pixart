59875a1c12bab93d3e011743dbc0c2a3e46bd333
Lyle Innes <lyleinnes@Lyles-MacBook-Pro.local>
2017-10-20 14:49:17 +1100

    commit 3

--- pixart.js
+++ pixart.js
@@ -3,7 +3,12 @@ $setColor = $('#set-color');
 $brush = $('.brush');
 
 
+
 $setColor.on('click', function() {
   event.preventDefault();
   $brush.css('background', $colorField.val());
 });
+
+for (i = 0; i < 1000; i++) {
+  $('body').append("<div class='square'>");
+}
